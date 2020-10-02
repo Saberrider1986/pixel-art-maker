d91e572ae5ee99ed62f26bf3d1bf7836a56e2133
Simon Wolf <simonw@hotmail.de>
2020-10-02 12:16:20 +0200

    Added a nearly functioning loop (-1 value as w)
    Grid is in current version v.02 able to draw a grid with 2 & coloring
    is possible as well. Still insertCell problem.

--- designs.js
+++ designs.js
@@ -1,36 +1,36 @@
 // Developer Simon Wolf
-// Date: 21.09.20 - v 0.1
+// Date: 21.09.20 - v 0.1 02.10.2020 v 0.2
 // Udacity Pixel Art Maker - Project
 
 // Grid information variables
 var size = document.getElementById('sizePicker');
-var canvas = document.getElementById('pixelCanvas');
 var table = document.getElementById('table');
-
+var canvas = document.getElementById('pixelCanvas');
 // Select color value input
-var color = document.getElementById('colorPicker').value;
-
+var color = document.getElementById('colorPicker');
 // Select size input
-const gridHeight = document.querySelector('#inputHeight').value;
-const gridWidth = document.querySelector('#inputWidth').value;
+var gridHeight = document.querySelector('#inputHeight').value;
+var gridWidth = document.querySelector('#inputWidth').value;
 
 // When size is submitted by the user, call makeGrid()
-size.addEventListener('submit', function (e) {
-  e.preventDefault();
+size.addEventListener('submit', function (event) {
+  event.preventDefault();
   makeGrid(gridHeight, gridWidth);
-})
+});
 
 // makeGrid Function details
-
-function makeGrid(x,y) {
+function makeGrid(gridHeight, gridWidth) {
 // Clear old grid first
-canvas.innerHTML = null;
-// Loop
-    for (var x = 1; x <= gridHeight; x++) {
-      var row = canvas.insertRow(); {
-        for (var y = 1; y <= gridWidth; y++);
-        row.insertCell();
-      };
+canvas.innerHTML = "";
+// Loop grid creation
+for (var h = 0; h <= gridHeight; h++) {
+var row = canvas.insertRow(h);
+    for (var w = 0; w <= gridWidth; w++); {
+        var cell = row.insertCell(-1);
+        cell.addEventListener('click', function (event) {
+          cell.style.backgroundColor = color.value;
+        })
     }
-  };
+  }
+}
 // Your code goes here!
