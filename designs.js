486bb79154a063caa908ac6f5a00781a81405fd1
Simon Wolf <simonw@hotmail.de>
2020-10-03 19:24:42 +0200

    Change global variables & create grid-fix

--- designs.js
+++ designs.js
@@ -2,19 +2,21 @@
 // Date: 21.09.20 - v 0.1 02.10.2020 v 0.2
 // Udacity Pixel Art Maker - Project
 
-// Grid information variables
 var size = document.getElementById('sizePicker');
-var table = document.getElementById('table');
 var canvas = document.getElementById('pixelCanvas');
+var table = document.getElementById('table');
 // Select color value input
 var color = document.getElementById('colorPicker');
 // Select size input
 var gridHeight = document.querySelector('#inputHeight').value;
 var gridWidth = document.querySelector('#inputWidth').value;
-
 // When size is submitted by the user, call makeGrid()
-size.addEventListener('submit', function (event) {
-  event.preventDefault();
+size.addEventListener('submit', function () {
+  gridHeight = document.querySelector('#inputHeight').value;
+  gridWidth = document.querySelector('#inputWidth').value;
+  //event.preventDefault();
+  // Grid information variables
+
   makeGrid(gridHeight, gridWidth);
 });
 
@@ -22,11 +24,14 @@ size.addEventListener('submit', function (event) {
 function makeGrid(gridHeight, gridWidth) {
 // Clear old grid first
 canvas.innerHTML = "";
+console.log("gridHeight, gridHeight")
+color = document.getElementById('colorPicker');
 // Loop grid creation
 for (var h = 0; h <= gridHeight; h++) {
 var row = canvas.insertRow(h);
-    for (var w = 0; w <= gridWidth; w++); {
-        var cell = row.insertCell(h);
+    for (var w = 0; w <= gridWidth; w++) {
+        var cell = row.insertCell(w);
+
         cell.addEventListener('click', function (event) {
           cell.style.backgroundColor = color.value;
         })
