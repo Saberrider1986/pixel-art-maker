f5012ab26251722f66805b5676d992bc030b86c8
Simon Wolf <simonw@hotmail.de>
2020-10-11 19:03:57 +0200

    Added nearly functioning Grid & colouring
    Adjustments inside the branch grid-fixv1 to create a function grid
    with makeGrid() function as well as the coloring, where Udacity
    Knowledge base helped a lot.
    
    issues #1 #4 #3 finished

--- designs.js
+++ designs.js
@@ -2,40 +2,30 @@
 // Date: 21.09.20 - v 0.1 02.10.2020 v 0.2
 // Udacity Pixel Art Maker - Project
 
+//DOM variables
 var size = document.getElementById('sizePicker');
 var canvas = document.getElementById('pixelCanvas');
-var table = document.getElementById('table');
-// Select color value input
 var color = document.getElementById('colorPicker');
-// Select size input
 var gridHeight = document.querySelector('#inputHeight').value;
 var gridWidth = document.querySelector('#inputWidth').value;
+
 // When size is submitted by the user, call makeGrid()
-size.addEventListener('submit', function () {
+size.addEventListener('submit', function (event) {
   gridHeight = document.querySelector('#inputHeight').value;
   gridWidth = document.querySelector('#inputWidth').value;
-  //event.preventDefault();
-  // Grid information variables
-
-  makeGrid(gridHeight, gridWidth);
-});
+  makeGrid(gridHeight, gridWidth)});
 
 // makeGrid Function details
 function makeGrid(gridHeight, gridWidth) {
-// Clear old grid first
+event.preventDefault();
 canvas.innerHTML = "";
-console.log("gridHeight, gridHeight")
-color = document.getElementById('colorPicker');
-// Loop grid creation
 for (var h = 0; h <= gridHeight; h++) {
-var row = canvas.insertRow(h);
+  var row = canvas.insertRow(h);
     for (var w = 0; w <= gridWidth; w++) {
-        var cell = row.insertCell(w);
-
-        cell.addEventListener('click', function (event) {
-          cell.style.backgroundColor = color.value;
-        })
+      var cell = row.insertCell(w);
+      canvas.addEventListener('click', function (event) {
+        event.target.style.backgroundColor = color.value;
+      });
     }
   }
 }
-// Your code goes here!
