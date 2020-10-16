70b87cf34b4e81f1a7e84cba3c5b3bdaf43b9790
Simon Wolf <simonw@hotmail.de>
2020-10-16 18:13:11 +0200

    Fix: Final issue 6 + submission finnish
    Fixing the values of the loops & variables h/w so the grid finally
    appears with the correct typed document.
    
    Added Comment-lines for submission self-explanatory code
    e.g. Coloring-cells etc.
    
    Solved issue 6.

--- designs.js
+++ designs.js
@@ -2,7 +2,7 @@
 // Date: 21.09.20 - v 0.1 02.10.2020 v 0.2
 // Udacity Pixel Art Maker - Project
 
-//DOM variables
+// DOM variables
 var size = document.getElementById('sizePicker');
 var canvas = document.getElementById('pixelCanvas');
 var color = document.getElementById('colorPicker');
@@ -16,14 +16,15 @@ size.addEventListener('submit', function () {
   makeGrid(gridHeight, gridWidth);
 });
 
-// makeGrid Function details
+// makeGrid Function details & loops for making the canvas
 function makeGrid(gridHeight, gridWidth) {
 event.preventDefault();
 canvas.innerHTML = "";
-for (var h = 0; h <= gridHeight; h++) {
+for (var h = 0; h < gridHeight; h++) {
   var row = canvas.insertRow(h);
-    for (var w = 0; w <= gridWidth; w++) {
+    for (var w = 0; w < gridWidth; w++) {
       var cell = row.insertCell(w);
+      // Coloring cells with click
       cell.addEventListener('click', function (event) {
         event.target.style.backgroundColor = color.value;
       });
